2b5c350acbaa642e31546acf665908edf0fe6280
valiant <vbshekert@hotmail.com>
2019-02-20 12:16:56 +0100

    end tutorial video
    tutorial video followed "How to make your own WordPress theme from scratch (2018)" by "Mr Digital".
    
    vidoe is from 6 nov 2018, around 3 months old as of writing.

--- footer.php
+++ footer.php
@@ -1,4 +1,17 @@
 <?php wp_footer(); ?>
 
+<footer class="sticky-bottom">
+    <div class="container">
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'footer-menu', // 'theme_location' refers to functions.php/3.0
+                'menu_class' => 'navigation', // adds class to the menu for style
+            )
+        );
+        ?>
+    </div>
+</footer>
+
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * fallback for non-specified php files in wordpress
+ * some parts are available to have standard from the wordpress install
+ */
+?>
